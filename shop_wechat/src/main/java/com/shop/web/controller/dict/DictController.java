c3a067f17974e220933cd9601f81f567b8233eb3
wangjie_glb <wangjie_glb@qq.com>
2018-09-02 13:59:14 +0800

    删除部分注释

--- shop_wechat/src/main/java/com/shop/web/controller/dict/DictController.java
+++ shop_wechat/src/main/java/com/shop/web/controller/dict/DictController.java
@@ -21,9 +21,6 @@ import com.shop.api.utils.constant.ConstantKey;
  * @Description:(用户Controller)   
  * @author: 王杰
  * @date:   2018年7月25日 下午2:02:49   
- *     
- * @Copyright: 2018 www.xiaxiangdian.com Inc. All rights reserved. 
- * 注意：本内容仅限于霞湘店内部传阅，禁止外泄以及用于其他的商业目
  */
 @Controller
 public class DictController extends BaseController {
